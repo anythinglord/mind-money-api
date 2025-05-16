1c54726f4ac1317a333f6dd969f94650c391ed8a
anythinglord <sjsalcedoa@unal.edu.co>
2025-05-16 06:57:38 -0500

    add functions to create and validate otp

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { prisma } from "../config/prisma";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { getRandomUID } from "../util";
+import { getRandomUID, generateOTP } from "../util";
 import { Resend } from 'resend';
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -100,10 +100,10 @@ export const login = async (req: Request, res: Response) => {
     }
 }
 
-export const verifyEmail = async (req: Request, res: Response) => {
+export const forgotPassword = async (req: Request, res: Response) => {
     try {
-        const { email } = req.body
-        
+        const { email, refresh } = req.body
+        console.log('temail.', email)
         // Verify if the account exists
         const existingUser = await prisma.user.findUnique({ where: { email } });
         
@@ -111,6 +111,7 @@ export const verifyEmail = async (req: Request, res: Response) => {
             res.status(400).json({ message: "account not found" });
             return
         }
+        const { otp, expiresAt } = generateOTP();
         resend.emails.send({
             from: 'onboarding@resend.dev',
             to: 'backups217@gmail.com',
@@ -118,22 +119,59 @@ export const verifyEmail = async (req: Request, res: Response) => {
             html: `
                 <h1>PASSWORD CHANGE REQUEST</h1>
                 <p>We have received a request to change the password for your account of MindMoney.</p>
-                <p>please insert the 123456 code to follow with the process</p>
+                <p>please insert the ${otp} code to follow with the process</p>
                 <p>This code will expire in 5 minutes. If you haven't requested a password change, 
                 please ignore this email and no changes will be made to your account.</p>
             `
         });
-        res.status(201).json({ message: "account found" })
+
+        await prisma.otpToken.upsert({
+            where: { email },
+            update: { otp, expiresAt },
+            create: { email, otp, expiresAt },
+        });
+
+        res.status(201).json({ message: "verification successfully", refresh: refresh })
     } catch (error) {
         res.status(500).json({ message: "Server error" })
     }
 }
 
-export const forgotpassword = async (_req: Request, res: Response) => {
+export const verifyOtp = async (req: Request, res: Response) => {
     try {
-        /* 
-            
-        */
+        const { email, token } = req.body
+
+        // Verify if the email exists
+        const existingToken = await prisma.otpToken.findFirst({ where: { email } });
+        if (!existingToken) {
+            res.status(400).json({ message: "proccess not found" });
+            return
+        }
+
+        if (!token || token !== existingToken.otp) {
+            res.status(400).json({ message: "wrong token" });
+            return;
+        }
+        if (existingToken.expiresAt < new Date()) {
+            res.status(400).json({ message: "token expired" });
+            return;
+        }
+
+        const tempToken = jwt.sign({ email: email }, process.env.JWT_SECRET as string, {
+            expiresIn: "1h",
+        });
+
+        res
+            .cookie('temp_token', tempToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'strict',
+                maxAge: 5 * 60 * 1000 // 5 minutes
+            })
+            .status(201).json({ 
+                message: 'otp verification successfully', 
+            });
+
     } catch (error) {
         res.status(500).json({ message: "Server error" })
     }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, verifySession, verifyEmail } from "../controllers/user.controller";
+import { signup, login, logout, verifySession, forgotPassword, verifyOtp } from "../controllers/user.controller";
 import { authenticate } from "../middlewares/auth.middleware";
 
 const userRouter = express.Router();
@@ -7,7 +7,8 @@ const userRouter = express.Router();
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
 userRouter.post("/logout", logout);
-userRouter.post("/verify_email", verifyEmail);
+userRouter.post("/forgot_password", forgotPassword);
+userRouter.post("/verify_otp", verifyOtp);
 userRouter.get("/verify_session", authenticate, verifySession)
 
 
